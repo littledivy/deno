ec17a5eeb7dbd6458173df095398907116280961
Divy Srivastava <dj.srivastava23@gmail.com>
2022-11-29 10:30:19 +0530

    more

--- ext/wasi/lib.rs
+++ ext/wasi/lib.rs
@@ -25,15 +25,6 @@ const FDFLAGS_APPEND: i32 = 0x0001;
 
 thread_local!(static RNG: RefCell<StdRng>  = RefCell::new(StdRng::from_entropy()));
 
-pub fn init() -> Extension {
-  Extension::builder()
-    .js(include_js_files!(
-      prefix "deno:ext/wasi",
-      "00_wasi.js",
-    ))
-    .build()
-}
-
 struct FileDescriptor {
   rid: ResourceId,
   type_: i32,
@@ -62,7 +53,6 @@ impl deno_core::Resource for WasiContext {
   }
 }
 
-#[op]
 fn op_wasi_create(
   state: &mut OpState,
   args: Vec<String>,
@@ -144,11 +134,15 @@ macro_rules! wasi {
   (
     $memory_arg:ident,
     $(fn $name:ident(
-      $($arg:ident: $arg_ty:ty),*,
+      $( $arg:ident : $arg_ty:ty ),*,
     ) -> $ret:ty {
       $($body:tt)*
     }),* $(,)?
   ) => {
+    fn wasm_ops() -> Vec<deno_core::OpDecl> {
+      vec![$($name::decl(),)*]
+    }
+
     $(
       #[op(wasm)]
       fn $name(
@@ -172,330 +166,332 @@ wasi! {
   ) -> i32 {
     ERRNO_SUCCESS
   },
+
   fn op_args_sizes_get(
     argc_offset: i32,
     argv_buffer_size_offset: i32,
   ) -> i32 {
     ERRNO_SUCCESS
-  }
-}
+  },
 
-#[op(wasm)]
-fn op_environ_get(
-  environ_offset: i32,
-  environ_buffer_offset: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_SUCCESS
-}
+  fn op_environ_get(
+    environ_offset: i32,
+    environ_buffer_offset: i32,
+  ) -> i32 {
+    ERRNO_SUCCESS
+  },
 
-#[op(wasm)]
-fn op_environ_sizes_get(
-  environ_count_offset: i32,
-  environ_size_offset: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_SUCCESS
-}
+  fn op_environ_sizes_get(
+    environ_count_offset: i32,
+    environ_size_offset: i32,
+  ) -> i32 {
+    ERRNO_SUCCESS
+  },
 
-#[op(wasm)]
-fn op_clock_res_get(
-  clock_id: i32,
-  resolution: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_SUCCESS
-}
+  fn op_clock_res_get(
+    clock_id: i32,
+    resolution: i32,
+  ) -> i32 {
+    ERRNO_SUCCESS
+  },
 
-#[op(wasm)]
-fn op_clock_time_get(
-  clock_id: i32,
-  precision: u64,
-  time: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_SUCCESS
-}
+  fn op_clock_time_get(
+    clock_id: i32,
+    precision: u64,
+    time: i32,
+  ) -> i32 {
+    ERRNO_SUCCESS
+  },
 
-#[op]
-fn op_fd_advise(_fd: i32, _offset: u64, _len: u64, _advice: i32) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_fdstat_get(_fd: i32, _buf: i32, ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_allocate(_fd: i32, _offset: u64, _len: u64) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_pread(
+    _fd: i32,
+    _iovs: i32,
+    _iovs_len: i32,
+    _offset: u64,
+    _nread: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_close(_fd: i32) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_prestat_get(_fd: i32, _buf: i32, ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_datasync(_fd: i32) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_prestat_dir_name(
+    _fd: i32,
+    _path: i32,
+    _path_len: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_fd_fdstat_get(_fd: i32, _buf: i32, memory: Option<&mut [u8]>) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_pwrite(
+    _fd: i32,
+    _ciovs: i32,
+    _ciovs_len: i32,
+    _offset: u64,
+    _nwritten: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_fdstat_set_flags(_fd: i32, _flags: i32) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_read(
+    _fd: i32,
+    _iovs: i32,
+    _iovs_len: i32,
+    _nread: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_fdstat_set_rights(
-  _fd: i32,
-  _fs_rights_base: u64,
-  _fs_rights_inheriting: u64,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_readdir(
+    _fd: i32,
+    _buf: i32,
+    _buf_len: i32,
+    _cookie: i64,
+    _bufused: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_filestat_get(_fd: i32, _buf: i32, memory: Option<&mut [u8]>) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_seek(
+    _fd: i32,
+    _offset: i64,
+    _whence: i32,
+    _newoffset: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_filestat_set_size(_fd: i32, _size: u64) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_tell(_fd: i32, _offset: i32, ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_filestat_set_times(
-  _fd: i32,
-  _atim: u64,
-  _mtim: u64,
-  _fst_flags: i32,
-) -> i32 {
-  ERRNO_NOSYS
-}
 
-#[op(wasm)]
-fn op_fd_pread(
-  _fd: i32,
-  _iovs: i32,
-  _iovs_len: i32,
-  _offset: u64,
-  _nread: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_write(
+    _fd: i32,
+    _ciovs: i32,
+    _ciovs_len: i32,
+    _nwritten: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_fd_prestat_get(_fd: i32, _buf: i32, memory: Option<&mut [u8]>) -> i32 {
-  ERRNO_NOSYS
-}
 
-#[op(wasm)]
-fn op_fd_prestat_dir_name(
-  _fd: i32,
-  _path: i32,
-  _path_len: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_create_directory(
+    _fd: i32,
+    _path_ptr: i32,
+    _path_len: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_fd_pwrite(
-  _fd: i32,
-  _ciovs: i32,
-  _ciovs_len: i32,
-  _offset: u64,
-  _nwritten: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
 
-#[op(wasm)]
-fn op_fd_read(
-  _fd: i32,
-  _iovs: i32,
-  _iovs_len: i32,
-  _nread: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_filestat_get(
+    _fd: i32,
+    _flags: i32,
+    _path_ptr: i32,
+    _path_len: i32,
+    _buf: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_fd_readdir(
-  _fd: i32,
-  _buf: i32,
-  _buf_len: i32,
-  _cookie: i64,
-  _bufused: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_link(
+    _old_fd: i32,
+    _old_flags: i32,
+    _old_path_ptr: i32,
+    _old_path_len: i32,
+    _new_fd: i32,
+    _new_path_ptr: i32,
+    _new_path_len: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_renumber(_from: i32, _to: i32) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_open(
+    _fd: i32,
+    _dirflags: i32,
+    _path_ptr: i32,
+    _path_len: i32,
+    _oflags: i32,
+    _fs_rights_base: u64,
+    _fs_rights_inheriting: u64,
+    _fdflags: i32,
+    _opened_fd: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_fd_seek(
-  _fd: i32,
-  _offset: i64,
-  _whence: i32,
-  _newoffset: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_readlink(
+    _fd: i32,
+    _path_ptr: i32,
+    _path_len: i32,
+    _buf: i32,
+    _buf_len: i32,
+    _bufused: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_fd_sync(_fd: i32) -> i32 {
-  ERRNO_NOSYS
-}
 
-#[op(wasm)]
-fn op_fd_tell(_fd: i32, _offset: i32, memory: Option<&mut [u8]>) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_remove_directory(
+    _fd: i32,
+    _path_ptr: i32,
+    _path_len: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_fd_write(
-  _fd: i32,
-  _ciovs: i32,
-  _ciovs_len: i32,
-  _nwritten: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
 
-#[op(wasm)]
-fn op_path_create_directory(
-  _fd: i32,
-  _path_ptr: i32,
-  _path_len: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_rename(
+    _old_fd: i32,
+    _old_path_ptr: i32,
+    _old_path_len: i32,
+    _new_fd: i32,
+    _new_path_ptr: i32,
+    _new_path_len: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_path_filestat_get(
-  _fd: i32,
-  _flags: i32,
-  _path_ptr: i32,
-  _path_len: i32,
-  _buf: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_symlink(
+    _old_path_ptr: i32,
+    _old_path_len: i32,
+    _fd: i32,
+    _new_path_ptr: i32,
+    _new_path_len: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op]
-fn op_path_filestat_set_times(
-  _fd: i32,
-  _flags: i32,
-  _path_ptr: i32,
-  _path_len: i32,
-  _atim: u64,
-  _mtim: u64,
-  _fst_flags: i32,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_path_unlink_file(
+    _fd: i32,
+    _path_ptr: i32,
+    _path_len: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_path_link(
-  _old_fd: i32,
-  _old_flags: i32,
-  _old_path_ptr: i32,
-  _old_path_len: i32,
-  _new_fd: i32,
-  _new_path_ptr: i32,
-  _new_path_len: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_random_get(
+    buffer_offset: i32,
+    buffer_len: i32,
+  ) -> i32 {
+    RNG.with(|rng| {
+      let rng = &mut rng.borrow_mut();
+      rng.fill(
+        &mut memory[buffer_offset as usize..(buffer_offset + buffer_len) as usize],
+      )
+    },);
 
-#[op(wasm)]
-fn op_path_open(
-  _fd: i32,
-  _dirflags: i32,
-  _path_ptr: i32,
-  _path_len: i32,
-  _oflags: i32,
-  _fs_rights_base: u64,
-  _fs_rights_inheriting: u64,
-  _fdflags: i32,
-  _opened_fd: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+    ERRNO_SUCCESS
+  },
 
-#[op(wasm)]
-fn op_path_readlink(
-  _fd: i32,
-  _path_ptr: i32,
-  _path_len: i32,
-  _buf: i32,
-  _buf_len: i32,
-  _bufused: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  // Non-memory ops
 
-#[op(wasm)]
-fn op_path_remove_directory(
-  _fd: i32,
-  _path_ptr: i32,
-  _path_len: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_renumber(_from: i32, _to: i32,) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_path_rename(
-  _old_fd: i32,
-  _old_path_ptr: i32,
-  _old_path_len: i32,
-  _new_fd: i32,
-  _new_path_ptr: i32,
-  _new_path_len: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
 
-#[op(wasm)]
-fn op_path_symlink(
-  _old_path_ptr: i32,
-  _old_path_len: i32,
-  _fd: i32,
-  _new_path_ptr: i32,
-  _new_path_len: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
-}
+  fn op_fd_fdstat_set_flags(_fd: i32, _flags: i32,) -> i32 {
+    ERRNO_NOSYS
+  },
 
-#[op(wasm)]
-fn op_path_unlink_file(
-  _fd: i32,
-  _path_ptr: i32,
-  _path_len: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  ERRNO_NOSYS
+
+  fn op_fd_fdstat_set_rights(
+    _fd: i32,
+    _fs_rights_base: u64,
+    _fs_rights_inheriting: u64,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
+
+
+  fn op_fd_filestat_get(_fd: i32, _buf: i32, ) -> i32 {
+    ERRNO_NOSYS
+  },
+
+
+  fn op_fd_filestat_set_size(_fd: i32, _size: u64,) -> i32 {
+    ERRNO_NOSYS
+  },
+
+
+  fn op_fd_filestat_set_times(
+    _fd: i32,
+    _atim: u64,
+    _mtim: u64,
+    _fst_flags: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
+
+
+  fn op_fd_advise(_fd: i32, _offset: u64, _len: u64, _advice: i32,) -> i32 {
+    ERRNO_NOSYS
+  },
+
+
+  fn op_fd_allocate(_fd: i32, _offset: u64, _len: u64,) -> i32 {
+    ERRNO_NOSYS
+  },
+
+
+  fn op_fd_close(_fd: i32,) -> i32 {
+    ERRNO_NOSYS
+  },
+
+
+  fn op_fd_datasync(_fd: i32,) -> i32 {
+    ERRNO_NOSYS
+  },
+
+
+  fn op_fd_sync(_fd: i32,) -> i32 {
+    ERRNO_NOSYS
+  },
+
+  fn op_path_filestat_set_times(
+    _fd: i32,
+    _flags: i32,
+    _path_ptr: i32,
+    _path_len: i32,
+    _atim: u64,
+    _mtim: u64,
+    _fst_flags: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
+
+  fn op_sock_recv(
+    _fd: i32,
+    _riDataOffset: i32,
+    _riDataLength: i32,
+    _riFlags: i32,
+    _roDataLengthOffset: i32,
+    _roFlagsOffset: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
+
+  fn op_sock_send(
+    _fd: i32,
+    _siDataOffset: i32,
+    _siDataLength: i32,
+    _siFlags: i32,
+    _soDataLengthOffset: i32,
+  ) -> i32 {
+    ERRNO_NOSYS
+  },
+
+  fn op_sock_shutdown(_fd: i32, _how: i32,) -> i32 {
+    ERRNO_NOSYS
+  },
 }
 
 #[op]
@@ -504,7 +500,7 @@ fn op_poll_oneoff() -> i32 {
 }
 
 #[op]
-fn op_proc_exit(rval: i32) {
+fn op_proc_exit(rval: i32) -> i32 {
   std::process::exit(0);
 }
 
@@ -518,49 +514,14 @@ fn op_sched_yield() -> i32 {
   ERRNO_SUCCESS
 }
 
-#[op(wasm)]
-fn op_random_get(
-  buffer_offset: i32,
-  buffer_len: i32,
-  memory: Option<&mut [u8]>,
-) -> i32 {
-  let mem = memory.unwrap();
-  RNG.with(|rng| {
-    let rng = &mut rng.borrow_mut();
-    rng.fill(
-      &mut mem[buffer_offset as usize..(buffer_offset + buffer_len) as usize],
-    )
-  });
-
-  ERRNO_SUCCESS
-}
-
-#[op]
-fn op_sock_recv(
-  _fd: i32,
-  _riDataOffset: i32,
-  _riDataLength: i32,
-  _riFlags: i32,
-  _roDataLengthOffset: i32,
-  _roFlagsOffset: i32,
-) -> i32 {
-  ERRNO_NOSYS
-}
-
-#[op]
-fn op_sock_send(
-  _fd: i32,
-  _siDataOffset: i32,
-  _siDataLength: i32,
-  _siFlags: i32,
-  _soDataLengthOffset: i32,
-) -> i32 {
-  ERRNO_NOSYS
-}
-
-#[op]
-fn op_sock_shutdown(_fd: i32, _how: i32) -> i32 {
-  ERRNO_NOSYS
+pub fn init() -> Extension {
+  Extension::builder()
+    .js(include_js_files!(
+      prefix "deno:ext/wasi",
+      "00_wasi.js",
+    ))
+    .ops(wasm_ops())
+    .build()
 }
 
 pub fn get_declaration() -> PathBuf {
